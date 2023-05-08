8d34488dbafb516fb0d19369ea38b95be48a747d
DITNO <92884446+DITNO@users.noreply.github.com>
2023-05-08 17:19:46 +0530

    Update calculator.js
    adding file fetch property and __dirname fuction

--- calculator.js
+++ calculator.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+//.sendFile to send the file over to the server
+//__dirname is used to fetch the file no matter the location
 app.get('/',function(req,res){
-    res.send('<h1>Calculator</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000);
