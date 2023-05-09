623238e515725f2193ff75bca8a71e9d05b00678
DITNO <92884446+DITNO@users.noreply.github.com>
2023-05-09 16:55:06 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 //.sendFile to send the file over to the server
 //__dirname is used to fetch the file no matter the location
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+app.post('/',function(req,res){
+    //number user here to conver the string obtained from the the req.body.num1 into integer so that it can operate on the result.
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send(result.toString());
+});
+;
+
+app.listen(3000,function(){
+    console.log('server is running');
+});
