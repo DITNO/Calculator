8a44a2c1dfa3b73142e95eb30e5a651db0347376
DITNO <92884446+DITNO@users.noreply.github.com>
2023-05-10 22:58:21 +0530

    Update calculator.js
    adding bmi server

--- calculator.js
+++ calculator.js
@@ -10,13 +10,21 @@ app.get('/',function(req,res){
 });
 
 app.post('/',function(req,res){
-    //number user here to conver the string obtained from the the req.body.num1 into integer so that it can operate on the result.
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
     var result = num1 + num2;
     res.send(result.toString());
 });
-;
+
+app.get('/bmicalculator',function(req,res){
+    res.sendFile(__dirname+'/bmiCalculator.html');
+});
+app.post('/bmicalculator',function(req,res){
+    var weight = Number(req.body.num1);
+    var height = Number(req.body.num2);
+    var BMI = weight/height;
+    res.send(BMI.toString());
+});
 
 app.listen(3000,function(){
     console.log('server is running');
